5e594f5cd3b9cc4da3d4ada1b2b71f4f3c61ee8d
D7460N <dragon@dragontheory.com>
2023-09-03 23:01:16 -0400

    disabled fade-in for now

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,19 @@
 // https://medium.com/@fbnlsr/how-to-get-rid-of-the-flash-of-unstyled-content-d6b79bf5d75f
 
 // Helper function
-let domReady = (cb) => {
-  document.readyState === 'interactive' || document.readyState === 'complete'
-    ? cb()
-    : document.addEventListener('DOMContentLoaded', cb);
-};
+//let domReady = (cb) => {
+//  document.readyState === 'interactive' || document.readyState === 'complete'
+//    ? cb()
+//    : document.addEventListener('DOMContentLoaded', cb);
+//};
 
-domReady(() => {
+//domReady(() => {
   // Display body when DOM is loaded
-  document.body.style.opacity = '1.0';
-});
+//  document.body.style.opacity = '1.0';
+//});
 
 
-
+/*
 
 // Function to update the theme color meta tag
 function updateThemeColor() {
@@ -45,3 +45,4 @@ observer.observe(document.documentElement, {
 
 // Update the theme color when the page loads
 window.addEventListener('load', updateThemeColor);
+*/
