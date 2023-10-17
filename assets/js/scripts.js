57807115727bbb36fa98e69f6267460ac9dbef81
DragonTheory <80736+dragontheory@users.noreply.github.com>
2023-10-17 08:50:14 -0400

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,48 +1,15 @@
-// https://medium.com/@fbnlsr/how-to-get-rid-of-the-flash-of-unstyled-content-d6b79bf5d75f
+const meta = document.querySelector('meta[name="color-scheme"]');
+let colorScheme = meta.content;  // Ensure colorScheme is defined here
 
-// Helper function
-//let domReady = (cb) => {
-//  document.readyState === 'interactive' || document.readyState === 'complete'
-//    ? cb()
-//    : document.addEventListener('DOMContentLoaded', cb);
-//};
-
-//domReady(() => {
-  // Display body when DOM is loaded
-//  document.body.style.opacity = '1.0';
-//});
-
-
-/*
-
-// Function to update the theme color meta tag
-function updateThemeColor() {
-  // Get the current theme color from the CSS variable
-  const currentThemeColor = getComputedStyle(document.documentElement)
-                            .getPropertyValue('--current-theme').trim();
-
-  // Find the theme-color meta tag
-  const metaThemeColor = document.querySelector("meta[name='theme-color']");
-
-  // Update the content attribute with the current theme color
-  metaThemeColor.setAttribute("content", currentThemeColor);
-}
-
-// Initialize MutationObserver
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-      updateThemeColor();
+document.querySelector('[role="switch"]').addEventListener('click', () => {
+    if (!window.frameElement) {
+      colorScheme = colorScheme === 'light' ? 'dark' : 'light';
+      document.documentElement.style.colorScheme = colorScheme;
+      meta.content = colorScheme;
+      if (window.frames[0]) {
+        const root = window.frames[0].document.documentElement;
+        root.style.colorScheme = colorScheme;
+        root.querySelector('meta[name="color-scheme"]').content = colorScheme;
+      }
     }
   });
-});
-
-// Observe changes to the :root element's style attribute
-observer.observe(document.documentElement, {
-  attributes: true,
-  attributeFilter: ['style']
-});
-
-// Update the theme color when the page loads
-window.addEventListener('load', updateThemeColor);
-*/
